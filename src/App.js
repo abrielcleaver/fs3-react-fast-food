95825460592f87ab6e331b7b826d2a696f451b6e
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-02 12:36:06 -0700

    Update dropdown components to pass the right id to the setDrinkId/setFoodId/setSideId function onChange. add components to App and now all 3 dropdowns render on page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-// import DrinkDropdown from './DrinkDropdown';
-// import FoodDropdown from './FoodDropdown';
-// import SideDropdown from './SideDropdown';
+import { useState } from 'react';
+import DrinkDropdown from './DrinkDropdown';
+import FoodDropdown from './FoodDropdown';
+import SideDropdown from './SideDropdown';
 // import InstructionsList from './InstructionsList';
 // import InstructionsForm from './FoodImages';
 // import Instruction from './Instruction';
@@ -13,7 +14,9 @@ import './App.css';
 function App() {
     // track some state here.
     // You'll need to keep track of a foodId, sideId, and drinkId. All these start out as 1
-
+  const [drinkId, setDrinkId] = useState(1);
+  const [foodId, setFoodId] = useState(1);
+  const [sideId, setSideId] = useState(1);
     // you'll need to track a order name, which starts as the order of your choice.
 
     // finally, you'll need an array of instructions, which could start out as [' '] for example
@@ -22,13 +25,22 @@ function App() {
     <div className="App">
       {/* here, the Order component takes in foodId, sideId, drinkId as props. It'll use those ids to render pictures correctly. */}
       {/* dynamically update the order name here using state */}
-
       {/* here, the OrderNameInput component takes in the setOrderName state handler function */}
+      {/* <OrderNameInput /> */}
       
       {/* 
           render all three Dropdown components (FoodDropdown, SideDropdown, DrinkDropdown) here. 
           In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state 
           */}
+      <div>
+        <DrinkDropdown setDrinkId={setDrinkId} />
+      </div>
+      <div>
+        <FoodDropdown setFoodId={setFoodId}/>
+      </div>
+      <div>
+        <SideDropdown setSideId={setSideId} />
+      </div>
 
       {/* here, the InstructionsForm component takes in the setInstructions state handler function and the slogans array that live in state */}
  
---
+++ src/DrinkDropdown.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+{/* this component should have a prop called setDrinkId, which is a function */}
+{/* on change, this component should use e.target.value to pass the correct id to the setDrinkId function */}
+{/* note that the options will need value properties (1, 2, or 3) that correspond to the photo filenames in the public directory */}
+export default function DrinkDropdown({ setDrinkId }) {
+  function handleChange(e) {
+    setDrinkId(e.target.value);
+  }
+   
+  return <div>
+    <select onChange={handleChange}>
+      <option>Water</option>
+      <option>Soda</option>
+      <option>Juice</option>
+    </select>
+  </div>;
+}
+
+
+
