4da3bbe8ce2ca6aed9b8abf69fa612750d1157e5
Abriel Cleaver <abrielcleaver@gmail.com>
2022-02-02 12:20:09 -0700

    Remove template information from App, create files for components and import into App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+// import DrinkDropdown from './DrinkDropdown';
+// import FoodDropdown from './FoodDropdown';
+// import SideDropdown from './SideDropdown';
+// import InstructionsList from './InstructionsList';
+// import InstructionsForm from './FoodImages';
+// import Instruction from './Instruction';
+// import NameInput from './OrderNameInput';
+
+
+
 
 function App() {
+    // track some state here.
+    // You'll need to keep track of a foodId, sideId, and drinkId. All these start out as 1
+
+    // you'll need to track a order name, which starts as the order of your choice.
+
+    // finally, you'll need an array of instructions, which could start out as [' '] for example
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* here, the Order component takes in foodId, sideId, drinkId as props. It'll use those ids to render pictures correctly. */}
+      {/* dynamically update the order name here using state */}
+
+      {/* here, the OrderNameInput component takes in the setOrderName state handler function */}
+      
+      {/* 
+          render all three Dropdown components (FoodDropdown, SideDropdown, DrinkDropdown) here. 
+          In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state 
+          */}
+
+      {/* here, the InstructionsForm component takes in the setInstructions state handler function and the slogans array that live in state */}
+ 
+      {/* here, the InstructionList component takes the array of slogans that lives in state */}
+
     </div>
   );
 }
